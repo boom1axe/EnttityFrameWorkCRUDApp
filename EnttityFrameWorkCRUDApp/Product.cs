0fade2c141f549b7090bc255031fb9202d786afa
Dennis Kafton <boom1axe@att.net>
2021-03-25 12:03:04 -0700

    Add project files.

---
+++ EnttityFrameWorkCRUDApp/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnttityFrameWorkCRUDApp
+{
+    class Product
+    {
+        public int productId { get; set; }
+
+        public string Title { get; set; }
+
+        public double Price { get; set; }
+    }
+}
